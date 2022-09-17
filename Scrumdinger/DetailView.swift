b93ea8fd069d6171ae56f4085e83cf2e809fa821
Kanishk Pachauri <krishna03012003@icloud.com>
2022-09-17 13:23:28 +0530

    added list view and button on homepage.

---
+++ Scrumdinger/DetailView.swift
@@ -0,0 +1,11 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by Krishna Pachauri on 17/09/22.
+//
+
+import Foundation
+import SwiftUI
+
+
--- Scrumdinger/Models/Theme.swift
+++ Scrumdinger/Models/Theme.swift
@@ -6,25 +6,24 @@
 //
 
 import SwiftUI
-import Foundation
 
 enum Theme: String {
     case bubblegum
-    case buttercup
-    case indigo
-    case lavender
-    case magenta
-    case navy
-    case orange
-    case oxblood
-    case periwinkle
-    case poppy
-    case purple
-    case seafoam
-    case sky
-    case tan
-    case teal
-    case yellow
+case buttercup
+case indigo
+case lavender
+case magenta
+case navy
+case orange
+case oxblood
+case periwinkle
+case poppy
+case purple
+case seafoam
+case sky
+case tan
+case teal
+case yellow
     
     var accentColor: Color {
         switch self {
